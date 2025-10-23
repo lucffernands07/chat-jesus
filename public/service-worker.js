e7af9543ca531ae621c6fc91bf21c50b27ef35ad
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-22 23:35:46 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,7 +13,7 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
-  self.skipWaiting();
+  
 });
 
 // Ativa e remove caches antigos
