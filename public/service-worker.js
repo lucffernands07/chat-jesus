950b749f8431504024b3f6e8aac7a0a8bd35384e
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-24 14:24:37 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,6 @@
 // Defina a versão aqui
 const version = 'v1'; // *** Troque para cada atualização
 
-const ASSETS_TO_CACHE = [
-  "/styles.css",
-  "/script.js",
-  "/script-salmos.js",
-  "/manifest.json",
-  "/icons/pray-128x.png",
-  "/icons/pray-512x.png",
-  "/img/image-bkg-ceu.jpeg"
-];
-
-
 // Recebe mensagem do front-end
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
